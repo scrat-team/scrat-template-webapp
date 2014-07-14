db4a30dc0aa37abfc67d1a3e8cd60588bc9ed8f7
Hinc Liu <hincubus@gmail.com>
2014-07-14 16:21:08 +0800

    更新 fis 配置

--- fis-conf.js
+++ fis-conf.js
@@ -2,4 +2,8 @@ var meta = require('./package.json');
 fis.config.set('name', meta.name);
 fis.config.set('version', meta.version);
 fis.config.set('project.exclude', 'node_modules/**');
-fis.config.set('framework.cache', true);
+fis.config.set('framework', {
+    cache: true,
+    urlPattern: '/c/%s',
+    comboPattern: '/co??%s'
+});
